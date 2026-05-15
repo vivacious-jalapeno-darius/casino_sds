6aa1e5d26dca59508139a0b46a991e51d66573af
Vivaan Jalla-Dhar <3602437@spsd.sk.ca>
2026-05-15 15:25:34 -0600

    Tried to fix cash display but did nothing

--- localstorage.js
+++ localstorage.js
@@ -6,6 +6,14 @@ function setup() {
   clearStorage('theme_red');
   clearStorage('theme_gold');
 
+  
+
+  if (cash !== undefined) {
+    cash = getItem('casino_cash');
+  }
+  else {
+    cash === undefined;
+  }
   cash = undefined;
   casinoRed = "#B30000";
   casinoGold = "#EFBF04";
